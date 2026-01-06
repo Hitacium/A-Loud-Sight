5d41a7bd36a00bebb99f26c45b46ca35840266a8
Hitacium <52213564+Hitacium@users.noreply.github.com>
2026-01-06 18:15:57 +0100

    Added level 1 prototype and changed scene routed

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	
 	
 func _play() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://scenes/level/level_1.tscn")
 
 
 func _settings() -> void:
